d46df6d120253c1581508e9df7e6142dbceff6b9
Willian <willianwelbert@yahoo.com.br>
2019-08-02 16:21:20 -0300

    adds collection page and items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Route, Switch} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop'component={ShopPage} />
       </Switch>
     </div>
   );
