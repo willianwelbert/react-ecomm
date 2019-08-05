c50e0df10766eefcd3f2cea2b4c5aaa63392074a
Willian <willianwelbert@yahoo.com.br>
2019-08-05 15:47:38 -0300

    implements email/password login

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props){
@@ -17,9 +17,14 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = (evt) => {
+  handleSubmit = async evt => {
     evt.preventDefault();
-    console.log('submit')
+    const {email, password} = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({email: '', password: ''})
+    } catch(error){ console.error(error.message) }
   }
 
   handleChange = (evt) => {
