6facdcf1e756679da012e7b8e8014a447dcf6d4d
Willian Welbert <willianwelbert@yahoo.com.br>
2019-08-12 23:27:16 -0300

    adds checkout item and implements checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,18 +1,32 @@
-import {createSelector} from 'reselect';
+import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCart],
-  (cart) => cart.cartItems
-)
+  cart => cart.cartItems
+);
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  cartItems => cartItems.reduce( (accQuantity, cartItem) => (accQuantity + cartItem.quantity), 0 )
-)
+  cartItems =>
+    cartItems.reduce(
+      (accQuantity, cartItem) => accQuantity + cartItem.quantity,
+      0
+    )
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accQuantity, cartItem) =>
+        accQuantity + cartItem.quantity * cartItem.price,
+      0
+    )
+);
 
 export const selectCartHidden = createSelector(
   [selectCart],
   cart => cart.hidden
-)
+);
