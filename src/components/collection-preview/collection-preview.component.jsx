d2a0dd1fa7392896c8308eeab66877eb770f28ed
Willian <willianwelbert@yahoo.com.br>
2019-08-13 18:03:25 -0300

    refactors shop data to its own reducer, respective selectors and new flow for directory/shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import './collection-preview.styles.scss';
-import CollectionItem from '../collection-item/collection-item.component';
+import React from "react";
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
-
-const CollectionPreview = ({title, items}) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {
-        items.filter((item, idx) => idx < 4  ).map( (item) => (
-          // <div key={item.id} >{item.name}</div>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
           <CollectionItem key={item.id} item={item} />
-        ))
-      }
+        ))}
     </div>
   </div>
-)
+);
 
-export default CollectionPreview
+export default CollectionPreview;
