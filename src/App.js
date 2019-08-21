315bac0ed1f0c93c3feadd514e7a0717de149cfe
Willian <willianwelbert@yahoo.com.br>
2019-08-21 19:19:59 -0300

    transfer data to firebase and sets up calls to fetch it

--- src/App.js
+++ src/App.js
@@ -1,69 +1,74 @@
-import React from 'react';
-import './App.css';
-import {connect} from 'react-redux';
-import {Route, Switch, Redirect} from 'react-router-dom';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import CheckoutPage from './pages/checkout/checkout.component';
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-import {createStructuredSelector} from 'reselect';
-import {selectCurrentUser} from './redux/user/user.selectors';
-import {setCurrentUser} from './redux/user/user.actions';
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-  
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
-  
-  componentDidMount(){
-    const {setCurrentUser} = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
-      if(userAuth){
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot( snapShot => {
-          setCurrentUser( {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          )
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
         });
-      }
-      else{
-        setCurrentUser(userAuth)
+      } else {
+        setCurrentUser(userAuth);
       }
     });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
-  
-  render(){
-    const {currentUser} = this.props;
+  render() {
+    const { currentUser } = this.props;
     return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop'component={ShopPage} />
-        <Route exact path='/signin' render={ () => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)  } />
-        <Route exact path='/checkout'component={CheckoutPage} />
-      </Switch>
-    </div>
-  );}
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+            }
+          />
+          <Route exact path="/checkout" component={CheckoutPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser : selectCurrentUser
-})
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch( setCurrentUser(user) )
-})
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyBttPt7BzAlp7ZbYOEKnr6J4ST98kN6EHk",
@@ -12,14 +12,16 @@ const config = {
   appId: "1:1072431427746:web:f6c366a63d4b505d"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
 
-  if(!snapShot.exists) {
-    const {displayName, email} = userAuth;
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
@@ -28,22 +30,51 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         email,
         createdAt,
         ...additionalData
-      })
-    } catch(error){
-      console.log('error creating user', error.message)
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
     }
   }
 
-  return userRef
-}
+  return userRef;
+};
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collectionsSnapshot => {
+  const transformedCollection = collectionsSnapshot.docs.map(docSnapshot => {
+    const { title, items } = docSnapshot.data();
+    return {
+      title,
+      items,
+      routeName: encodeURI(title.toLowerCase()),
+      id: docSnapshot.id
+    };
+  });
+
+  return transformedCollection.reduce((acc, collection) => {
+    acc[collection.title.toLowerCase()] = collection;
+    return acc;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
